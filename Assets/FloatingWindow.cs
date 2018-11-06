0d63bbbdd3ed1df8462a6c181b25972d47dca654
russellxie7 <waxie@ucsd.edu>
2018-11-05 20:07:11 -0800

    improved floating window animation

--- Assets/FloatingWindow.cs
+++ Assets/FloatingWindow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FloatingWindow : MonoBehaviour {
 
     public float X_range = 0.2f, Y_range = 0.3f;
+    public float animation_time = 1f;
 
     private float X_left, X_right, Y_up, Y_down;
 
@@ -12,10 +13,13 @@ public class FloatingWindow : MonoBehaviour {
     private float frame_counter;
     private Vector3 down_des, up_des;
     private bool isUp;
-    private float threshold_time; 
+    private float threshold_time;
+    private Vector3 initial_localPos;
+    private Vector3 initial_localScale;
+    private bool isAnimating;
 
-	// Use this for initialization
-	void Start () {
+    private void Awake()
+    {
         X_left = transform.localPosition.x - X_range;
         X_right = transform.localPosition.x + X_range;
         Y_up = transform.localPosition.y + Y_range;
@@ -27,11 +31,23 @@ public class FloatingWindow : MonoBehaviour {
         down_des = new Vector3(Random.Range(X_left, X_right), Y_down, z);
         isUp = true;
         threshold_time = Random.Range(2, 4);
+
+        isAnimating = false;
+
+        initial_localPos = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
+        initial_localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
+    }
+
+    // Use this for initialization
+    void Start () {
+
+
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (gameObject.activeSelf){
+        if (gameObject.activeSelf && !isAnimating){
             frame_counter += Time.deltaTime;
 
             if (frame_counter > threshold_time)
@@ -53,4 +69,41 @@ public class FloatingWindow : MonoBehaviour {
             }
         }
 	}
+
+    private void OnEnable()
+    {
+        DebugText.Instance.Log("initial pos: " + initial_localPos);
+
+        if(!isAnimating){
+            StartCoroutine(Bloom());
+        }
+        
+    }
+
+    IEnumerator Bloom(){
+
+        isAnimating = true;
+
+        transform.localScale = Vector3.zero;
+        transform.localPosition = Vector3.zero;
+
+        float time_passed = 0;
+
+        while (time_passed < animation_time){
+
+            transform.localScale = Vector3.Lerp(transform.localScale, initial_localScale, 0.1f);
+            transform.localPosition = Vector3.Lerp(transform.localPosition, initial_localPos, 0.1f);
+
+            time_passed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localScale = initial_localScale;
+        transform.localPosition = initial_localPos;
+
+        isAnimating = false;
+
+        yield return null;
+    }
 }
