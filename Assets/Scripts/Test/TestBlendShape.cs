3e73cbd362321e47415199129ca27aafb18ebf7c
Yue Wu <yuw264@ucsd.edu>
2019-04-24 22:59:41 -0700

    Particle systems part 1

---
+++ Assets/Scripts/Test/TestBlendShape.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Source: https://docs.unity3d.com/Manual/BlendShapes.html
+/// </summary>
+public class TestBlendShape : MonoBehaviour {
+    public SkinnedMeshRenderer skinnedMeshRenderer;
+    [Range(0, 100)]
+    public float blendProgress;
+
+    private readonly float blendSpeed = 1f;
+
+    void Awake() {
+        if (skinnedMeshRenderer == null) {
+            skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+        }
+    }
+
+    //void Update() {
+    //    if (blendOne < 100f) {
+    //        skinnedMeshRenderer.SetBlendShapeWeight(0, blendOne);
+    //        blendOne += blendSpeed;
+    //    }
+    //}
+
+    private void OnValidate() {
+        skinnedMeshRenderer.SetBlendShapeWeight(0, blendProgress);
+    }
+}
