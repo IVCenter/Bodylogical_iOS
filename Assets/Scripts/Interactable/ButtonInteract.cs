6140ade54147e03ca565f33496f9e6d9f46cad40
Yue Wu <yuw264@ucsd.edu>
2019-05-22 22:36:42 -0700

    Button tooltips

--- Assets/Scripts/Interactable/ButtonInteract.cs
+++ Assets/Scripts/Interactable/ButtonInteract.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.Events;
 
 public class ButtonInteract : MonoBehaviour, IInteractable {
@@ -7,8 +8,24 @@ public class ButtonInteract : MonoBehaviour, IInteractable {
 
     private Color? originalColor;
 
-    public virtual void OnCursorEnter() {
-        //Debug.Log("Cursor Entered");
+    // Tooltip
+    [Header("If no tooltip needed, leave this blank.")]
+    public string tooltipTextId;
+    public LocalizedParam[] tooltipTextParams;
+
+    public GameObject tooltipPrefab;
+    private GameObject tooltip;
+    private IEnumerator tooltipAnim;
+
+    // Following three variables in seconds
+    private static readonly float showTime = 2.0f;
+    private static readonly float hideTime = 0.5f;
+    private static readonly float animationTime = 0.5f;
+
+    private float origX;
+    private float origY;
+
+    public void OnCursorEnter() {
         if (gameObject.GetComponent<MeshRenderer>()) {
             if (originalColor == null) {
                 originalColor = gameObject.GetComponent<MeshRenderer>().material.color;
@@ -16,30 +33,84 @@ public class ButtonInteract : MonoBehaviour, IInteractable {
 
             gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
         }
+
+        if (!string.IsNullOrEmpty(tooltipTextId) && tooltip == null) {
+            tooltip = Instantiate(tooltipPrefab, transform.parent);
+            tooltip.transform.localPosition = new Vector3(0, 0, -GetComponent<Collider>().bounds.size.y / 2.0f - 3.0f);
+            tooltip.transform.GetChild(0).GetChild(0).GetComponent<LocalizedText>().SetText(tooltipTextId, tooltipTextParams);
+            tooltip.SetActive(false);
+        }
+
+        if (tooltip != null) {
+            if (tooltipAnim != null) {
+                StopCoroutine(tooltipAnim);
+            }
+            tooltipAnim = TooltipShow();
+            StartCoroutine(tooltipAnim);
+        }
     }
 
-    public virtual void OnCursorExited() {
-        //Debug.Log("Cursor Exited");
+    public void OnCursorExited() {
         if (GetComponent<MeshRenderer>() && originalColor != null) {
             GetComponent<MeshRenderer>().material.color = (Color)originalColor;
             originalColor = null;
+        }
+
+        if (tooltip != null) {
+            if (tooltipAnim != null) {
+                StopCoroutine(tooltipAnim);
+            }
+
+            // OnCursorExited might be triggered when the button gets hidden.
+            // In this case coroutines cannot be started, but we still wish to hide the tooltip.
+            if (gameObject.activeInHierarchy) {
+                tooltipAnim = TooltipHide();
+                StartCoroutine(tooltipAnim);
+            } else {
+                tooltip.SetActive(false);
+            }
         }
     }
 
     public virtual void OnScreenTouch(Vector2 coord) {
         clicked.Invoke();
-        //DebugText.Instance.Log("Touched on object: " + gameObject.name);
-        //DebugText.Instance.Log("Touched coord is : " + coord);
     }
 
-    public void OnScreenPress(Vector2 coord, float deltaTime, float pressure) {
-        //DebugText.Instance.Log("Pressed on object: " + gameObject.name);
-        //DebugText.Instance.Log("Pressed pressure: " + pressure);
+    public void OnScreenPress(Vector2 coord, float deltaTime, float pressure) {}
+    public void OnScreenTouchMoved(Vector2 coord, Vector2 deltaPosition) {}
+    public void OnScreenLeave(Vector2 coord) {}
+
+    /// <summary>
+    /// Displays the tooltip.
+    /// </summary>
+    private IEnumerator TooltipShow() {
+        yield return new WaitForSeconds(showTime);
+        tooltip.SetActive(true);
+        // For canvases z-scale doesn't matter
+        origX = tooltip.transform.localScale.x;
+        origY = tooltip.transform.localScale.y;
+
+        tooltip.transform.localScale = new Vector3(0, 0, 1);
+        int stepCount = (int)(animationTime / Time.deltaTime);
+        for (int i = 1; i <= stepCount; i++) {
+            tooltip.transform.localScale = new Vector3(i * origX / stepCount,
+                i * origY / stepCount, 1.0f);
+            yield return null;
+        }
     }
 
-    public void OnScreenTouchMoved(Vector2 coord, Vector2 deltaPosition) {
-        //DebugText.Instance.Log("Touch moved delta " + deltaPosition);
+    /// <summary>
+    /// Hides the tooltip.
+    /// </summary>
+    private IEnumerator TooltipHide() {
+        yield return new WaitForSeconds(hideTime);
+        // For canvases z-scale doesn't matter
+        int stepCount = (int)(animationTime / Time.deltaTime);
+        for (int i = stepCount - 1; i >= 0; i--) {
+            tooltip.transform.localScale = new Vector3(i * origX / stepCount, i * origY / stepCount, 1.0f);
+            yield return null;
+        }
+        tooltip.SetActive(false);
+        tooltip.transform.localScale = new Vector3(origX, origY, 1);
     }
-
-    public void OnScreenLeave(Vector2 coord) { }
 }
