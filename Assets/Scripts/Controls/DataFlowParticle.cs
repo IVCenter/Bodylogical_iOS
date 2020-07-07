57f47b23d0ceeb5c66efca14159d0a04ad8ae52a
Yue Wu <yuw264@ucsd.edu>
2020-07-07 14:40:45 -0700

    Basic particle demo

--- Assets/Scripts/Controls/DataFlowParticle.cs
+++ Assets/Scripts/Controls/DataFlowParticle.cs
@@ -1,17 +1,29 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DataFlowParticle : MonoBehaviour {
-    private ParticleSystem system;
-
-    // Start is called before the first frame update
-    void Start() {
-
+    [SerializeField] private Vector3[] positions;
+    [SerializeField] private float speed;
+    
+    private void Start() {
+        StartCoroutine(Travel());
     }
 
-    // Update is called once per frame
-    void Update() {
-
+    private IEnumerator Travel() {
+        while (true) {
+            for (int i = 0; i < positions.Length - 1; i++) {
+                transform.position = positions[i];
+                yield return null;
+                
+                float traveledDist = 0;
+                float totalDist = Vector3.Distance(positions[i], positions[i + 1]);
+                Vector3 dir = Vector3.Normalize(positions[i + 1] - positions[i]);
+                while (traveledDist < totalDist) {
+                    transform.position += dir * speed;
+                    traveledDist += speed;
+                    yield return null;
+                }
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Test/TestParticles.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TestParticles : MonoBehaviour {
+    [SerializeField] private float radius;
+    [SerializeField] private float speed;
+
+    private float frameSpeed;
+    private void Start() {
+        Application.targetFrameRate = 60;
+        frameSpeed = speed * Time.deltaTime;
+    }
+    private void Update() {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = radius;
+        Vector3 pos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector3 dir = Vector3.Normalize(pos - transform.position);
+        transform.position += dir * frameSpeed;
+    }
+}
