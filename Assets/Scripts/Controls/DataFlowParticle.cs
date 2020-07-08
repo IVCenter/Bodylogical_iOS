16c58d75114a8ba0aee65d2979956d7bab6f8c59
Yue Wu <yuw264@ucsd.edu>
2020-07-08 15:49:17 -0700

    Updated color for particle

--- Assets/Scripts/Controls/DataFlowParticle.cs
+++ Assets/Scripts/Controls/DataFlowParticle.cs
@@ -1,20 +1,36 @@
 ﻿using System.Collections;
 using UnityEngine;
 
+/// <summary>
+/// Particle effect that would travel along a route.
+/// </summary>
 public class DataFlowParticle : MonoBehaviour {
     [SerializeField] private Vector3[] positions;
     [SerializeField] private float speed;
+    [SerializeField] private ParticleSystem trail;
+    [SerializeField]private Color[] colors;
     
+    private ParticleSystem.MainModule module;
+    private IEnumerator travel;
+
     private void Start() {
-        StartCoroutine(Travel());
+        module = trail.main;
+        travel = Travel();
+        StartCoroutine(travel);
     }
 
     private IEnumerator Travel() {
+        HealthStatus status = HealthUtil.CalculateStatus(HealthLoader.Instance
+            .ChoiceDataDictionary[TimeProgressManager.Instance.Path].CalculateHealth(
+                TimeProgressManager.Instance.YearValue,
+                ArchetypeManager.Instance.Selected.ArchetypeData.gender));
+        module.startColor = colors[(int) status];
+
         while (true) {
             for (int i = 0; i < positions.Length - 1; i++) {
                 transform.position = positions[i];
                 yield return null;
-                
+
                 float traveledDist = 0;
                 float totalDist = Vector3.Distance(positions[i], positions[i + 1]);
                 Vector3 dir = Vector3.Normalize(positions[i + 1] - positions[i]);
@@ -24,6 +40,11 @@ public class DataFlowParticle : MonoBehaviour {
                     yield return null;
                 }
             }
+
+            // Move the particle to the beginning of the route
+            gameObject.SetActive(false);
+            transform.position = positions[0];
+            yield return new WaitForSeconds(5);
         }
     }
 }
