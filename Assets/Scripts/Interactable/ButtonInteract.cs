9a913ec12b426e1f77a9597e224fdbfc26d63419
Yue Wu <yuw264@ucsd.edu>
2020-09-10 19:39:25 -0700

    Removed button animations

--- Assets/Scripts/Interactable/ButtonInteract.cs
+++ Assets/Scripts/Interactable/ButtonInteract.cs
@@ -3,28 +3,25 @@ using UnityEngine.Events;
 
 public class ButtonInteract : Interactable {
     [SerializeField] private UnityEvent clicked;
-
+    private MeshRenderer meshRenderer;
     private const float dark = 0.4f;
     private static readonly Color darkColor = new Color(dark, dark, dark, 0f);
 
-    // Animation
-    [SerializeField] private ComponentAnimation buttonAnimation;
-
+    private void Awake() {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+    
     public override void OnTouchDown() {
-        if (gameObject.GetComponent<MeshRenderer>()) {
-            gameObject.GetComponent<MeshRenderer>().material.color -= darkColor;
+        if (meshRenderer != null) {
+            meshRenderer.material.color -= darkColor;
         }
     }
 
     public override void OnTouchUp() {
-        if (GetComponent<MeshRenderer>()) {
-            GetComponent<MeshRenderer>().material.color += darkColor;
+        if (meshRenderer != null) {
+           meshRenderer.material.color += darkColor;
         }
 
-        if (buttonAnimation != null && !buttonAnimation.IsAnimating) {
-            buttonAnimation.Invoke(clicked.Invoke);
-        } else if (buttonAnimation == null) {
-            clicked.Invoke();
-        }
+        clicked.Invoke();
     }
 }
